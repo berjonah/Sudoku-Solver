d9213f025415a219d688d0e1d520729bde7f0915
Andrew Fillmore <fillmoreandrew110@gmail.com>
2015-06-10 16:28:46 -0600

    ExclusiveZone
    Added abstract class ExclusiveZone.
    Contains an array of squares, a constructor, and testExclusivity, a
    method that tests to see if all the values in the zone are unique

---
+++ Sudoku Solver/Sudoku Solver/ExclusiveZone.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Solver
+{
+    abstract class ExclusiveZone
+    {
+        //has 9 squares
+        private Square[] squares;
+        public Square[] Squares
+        {
+            get
+            {
+                return squares;
+            }
+            set
+            {
+                squares = value;
+            }
+        }
+
+        public ExclusiveZone(Square[] inSquares)
+        {
+            if(inSquares.Length!=9)
+            {
+                throw new ArgumentOutOfRangeException("Argument must contain array of 9 squares");
+            }
+            else
+            {
+                Squares = inSquares;
+            }
+        }
+        bool testExclusivity()
+        {
+            List<int> found = new List<int>();
+            foreach(Square test in Squares)
+            {
+                if(test.Solved)
+                {
+                    if(!found.Contains(test.Number))
+                    {
+                        found.Add(test.Number);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
--- Sudoku Solver/Sudoku Solver/Square.cs
+++ Sudoku Solver/Sudoku Solver/Square.cs
@@ -8,8 +8,50 @@ namespace Sudoku_Solver
 {
     class Square
     {
-        private bool set;
+        private bool solved;
+        public bool Solved
+        {
+            get
+            {
+                return solved;
+            }
+            set
+            {
+                solved = value;
+            }
+        }
+
         private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                if(value < 1 || value > 9)
+                {
+                    throw new ArgumentOutOfRangeException("Value must be between 1 and 9");
+                }
+                else
+                {
+                    number = value;
+                }
+            }
+        }
+
         private int[] posibilities;
+
+        public Square(bool solved)
+        {
+            this.Solved = solved;
+        }
+
+        public Square(bool solved, int number)
+            : this(solved)
+        {
+            this.Number = number;
+        }
     }
 }
